46222b3a889afd8f0b9d652cb2cfaab04a24bf04
ChandeepaF <146319167+ChandeepaF@users.noreply.github.com>
2023-10-20 18:07:56 +1100

    Updated the fxml files for Add a new post & Dashboard

--- Assignment_2/src/Controller/DashboardController.java
+++ Assignment_2/src/Controller/DashboardController.java
@@ -1,32 +1,60 @@
 package Controller;
 
+import Model.TestModel;
+import Model.Exceptions.Invalid_Password_Exception;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class DashboardController {
 
 	@FXML
-	private Label changedetailsLabel;
-	
-	@FXML
-	private Label addpostLabel;
-	
-	@FXML
-	private Label exportpostLabel;
-	
-	@FXML
-	private Label removepostLabel;
-	
-	@FXML
-	private Label retrievepostLabel;
-	
-	@FXML
-	private Label retrievelikesLabel;
-	
-	@FXML
-	private Label logoutLabel;
-	
-	@FXML
 	private Label nameLabel;
+	
+//	nameLabel.setText(name);
+	
+	@FXML
+	public void changedetailsButtonHandler(ActionEvent event) {
+		
+		
+	}
+	
+	@FXML
+	public void addpostButtonHandler(ActionEvent event) {
+		
+		
+	}
+	
+	@FXML
+	public void retrievepostButtonHandler(ActionEvent event) {
+		
+		
+	}
+	
+	@FXML
+	public void retrievelikesButtonHandler(ActionEvent event) {
+		
+		
+	}
+	
+	@FXML
+	public void removepostButtonHandler(ActionEvent event) {
+		
+		
+	}
+	
+	@FXML
+	public void exportpostButtonHandler(ActionEvent event) {
+		
+		
+	}
+	
+	@FXML
+	public void logoutButtonHandler(ActionEvent event) {
+		
+		
+	}
+	
 	
 }
